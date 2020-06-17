53b795ca382ed511fb4171b2c7b7086245ed6e7f
Fanilo Andriamarohaja <fanilo.and@onja.org>
2020-06-17 09:03:19 +0100

    unfinished code

--- script.js
+++ script.js
@@ -1,2 +1,47 @@
 // write your code here
 console.log('it works!');
+
+const socialProgram = [
+    {
+      title: "Confidence",
+      link: '"http://confidence.com"',
+      author: "Jonshon"
+    },
+    
+    {
+      title: "Motivation",
+      link: '"https://www.motivation.com"',
+      author: "Ariane"
+    },
+    
+    {
+      title: "Diligence",
+      link: '"http://diligence.org"',
+      author: "Sonia"
+    }
+];
+  
+const menuChoices = () => {
+    for(let i=0; i<socialProgram.length; i++) {
+       let firstChoice = `${socialProgram[i].title} \n ${socialProgram[i].link} \n ${socialProgram[i].author}`;
+       alert(firstChoice)
+    }
+}
+
+const listOfChoices = `Choose an option:
+1: Show links
+2: Add a link
+3: Remove link
+0: Quit
+`;
+    
+let choice = Number(prompt(listOfChoices));
+
+while (choice !== 0) {
+  if (choice == 1) {
+      menuChoices()
+  }
+
+//   else if (choice == 2) {}
+
+}
